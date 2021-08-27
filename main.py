ec550e443296fb7a486eeb2c0e23171ca65a9df6
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-08-27 17:14:49 +0300

    Overall statistics for all cases implemented in main.py

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.log.util import interval_lifecycle
 from pm4py.statistics.concurrent_activities.pandas import get as concurrent_activities_get
 
-log = xes_importer.apply('data/PurchasingExampleModified.xes')
+event_log_path = 'data/PurchasingExampleModified.xes'
+log = xes_importer.apply(event_log_path)
+
+# Converting lifecycle log to interval log
 log_interval = interval_lifecycle.to_interval(log)
+
+# Conversion to pd.DataFrame format
 event_log_interval = log_converter.apply(log_interval, variant=log_converter.Variants.TO_DATA_FRAME)
+
+# Adjusting time slightly to avoid unnecessary concurrent events
+event_log_interval['start_timestamp'] = event_log_interval['start_timestamp'] + pd.Timedelta('1us')
+
+# handoff_id uniquely identifies a handoff
+handoff_id = ['source_activity', 'source_resource', 'destination_activity', 'destination_resource']
 
 
 # Transportation: Hand-off
@@ -15,6 +26,8 @@ event_log_interval = log_converter.apply(log_interval, variant=log_converter.Var
 # - duration
 # - score = frequency * duration
 def calculate_handoff_per_case(case: pd.DataFrame) -> pd.DataFrame:
+    global handoff_id
+
     case = case.sort_values(by='time:timestamp')
 
     # Processing concurrent activities
@@ -28,10 +41,8 @@ def calculate_handoff_per_case(case: pd.DataFrame) -> pd.DataFrame:
         concurrent = case[(case['Activity'] == activities_pair[0]) | (case['Activity'] == activities_pair[1])]
         case_without_concurrent_activities.drop(concurrent.index, inplace=True)
         if concurrent.iloc[0]['time:timestamp'] >= concurrent.iloc[1]['time:timestamp']:
-            print(f"Adding \"{concurrent.iloc[0]['Activity']}\", dropping \"{concurrent.iloc[1]['Activity']}\"")
             case_without_concurrent_activities = case_without_concurrent_activities.append(concurrent.iloc[0])
         else:
-            print(f"Adding \"{concurrent.iloc[1]['Activity']}\", dropping \"{concurrent.iloc[0]['Activity']}\"")
             case_without_concurrent_activities = case_without_concurrent_activities.append(concurrent.iloc[1])
     case_without_concurrent_activities.sort_values(by='start_timestamp', inplace=True)
     case_without_concurrent_activities.reset_index(drop=True, inplace=True)
@@ -55,7 +66,7 @@ def calculate_handoff_per_case(case: pd.DataFrame) -> pd.DataFrame:
     # Frequency
     handoff_per_case = pd.DataFrame(
         columns=['source_activity', 'source_resource', 'destination_activity', 'destination_resource', 'duration'])
-    grouped = handoff.groupby(by=['source_activity', 'source_resource', 'destination_activity', 'destination_resource'])
+    grouped = handoff.groupby(by=handoff_id)
     for group in grouped:
         pair, records = group
         handoff_per_case = handoff_per_case.append(pd.Series({
@@ -71,12 +82,29 @@ def calculate_handoff_per_case(case: pd.DataFrame) -> pd.DataFrame:
 
 
 event_log_interval_by_case = event_log_interval.groupby(by='case:concept:name')
+
+print('Calculating handoff per case...')
+results_per_case = {}
 for (case_id, case) in event_log_interval_by_case:
-    if case_id not in ['10']:
-        continue
-    print(case_id)
     result = calculate_handoff_per_case(case)
-    print(result)
+    results_per_case[case_id] = result
+
+statistics = pd.DataFrame(columns=handoff_id + ['frequency', 'duration'])
+
+print('Post-processing final statistics...')
+results_per_case_grouped = pd.concat(results_per_case, ignore_index=True).groupby(by=handoff_id)
+for group in results_per_case_grouped:
+    pair, records = group
+    frequency = records['frequency'].sum()  # len(records) == frequency
+    duration = records['duration'].sum()
+    statistics = statistics.append(pd.Series({
+        'source_activity': pair[0],
+        'source_resource': pair[1],
+        'destination_activity': pair[2],
+        'destination_resource': pair[3],
+        'duration': duration,
+        'frequency': frequency
+    }), ignore_index=True)
 
 # DONE: Sequential events
 #   - identical dates
@@ -92,8 +120,7 @@ for (case_id, case) in event_log_interval_by_case:
 #   - only interval event logs, i.e., event logs where each event has a start timestamp and a completion timestamp
 
 # TODO: Ping-pong handoff is not identified yet
-
 # TODO: CTE, processing time / full time
 #   - Do we count only business hours? Yes. Using 24 hours for PurchasingExample.xes
-# TODO: Add total handoff frequency, frequency per case using unique pairs source+resource
+# DONE: Add total handoff frequency, frequency per case using unique pairs source+resource
 # TODO: Mark manually some events with resource_type label "system" to add handoff type identification
