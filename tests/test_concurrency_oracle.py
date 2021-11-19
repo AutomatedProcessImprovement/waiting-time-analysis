8d46ecf6d01bccec8d0c4f8d1178cb5d8f85f518
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-11-19 20:52:30 +0200

    Adding a test for heuristic on a dataframe, #7

--- tests/test_concurrency_oracle.py
+++ tests/test_concurrency_oracle.py
@@ -1,7 +1,9 @@
 from config import Configuration, DEFAULT_XES_IDS, ConcurrencyOracleType, ResourceAvailabilityType, \
-    HeuristicsThresholds, ReEstimationMethod
-from event_log.concurrency_oracle import HeuristicsConcurrencyOracle as EventLogHeuristics
+    HeuristicsThresholds, ReEstimationMethod, EventLogIDs
+from data_frame.concurrency_oracle import HeuristicsConcurrencyOracle as DFHeuristics
+from event_log.concurrency_oracle import HeuristicsConcurrencyOracle as ELHeuristics
 from event_log_readers import read_event_log
+from waste import core
 
 
 def test_pm4py_heuristics(assets_path):
@@ -17,6 +19,33 @@ def test_pm4py_heuristics(assets_path):
     )
 
     log = read_event_log(str(log_path), config)
-    oracle = EventLogHeuristics(log, config)
+    oracle = ELHeuristics(log, config)
+
+    assert oracle is not None
+
+
+def test_data_frame_heuristics(assets_path):
+    log_path = assets_path / 'Production.xes'
+    log = core.lifecycle_to_interval(log_path)
+
+    column_names = EventLogIDs(
+        case='case:concept:name',
+        activity='concept:name',
+        start_timestamp='start_timestamp',
+        end_timestamp='time:timestamp',
+        resource='org:resource',
+        lifecycle='lifecycle:transition'
+    )
+
+    config = Configuration(
+        log_ids=column_names,
+        re_estimation_method=ReEstimationMethod.MODE,
+        concurrency_oracle_type=ConcurrencyOracleType.HEURISTICS,
+        resource_availability_type=ResourceAvailabilityType.SIMPLE,
+        bot_resources={"Start", "End"},
+        heuristics_thresholds=HeuristicsThresholds(df=0.9, l2l=0.9)
+    )
+
+    oracle = DFHeuristics(log, config)
 
     assert oracle is not None
