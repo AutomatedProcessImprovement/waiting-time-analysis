89790781e9169fe06e1b5df2451024219eaab209
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-12-03 18:11:32 +0200

    #4 Ping-pong identification function and tests

--- src/waste/handoff.py
+++ src/waste/handoff.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 import multiprocessing
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional
 
 import click
 import numpy as np
@@ -42,6 +42,74 @@ def identify(log_path: Path, parallel_run=True) -> pd.DataFrame:
     result = _join_per_case_handoffs(all_handoffs)
     result['duration_sum_seconds'] = result['duration_sum'] / np.timedelta64(1, 's')
     return result
+
+
+def _identify_ping_pongs_per_case(case: pd.DataFrame, parallel_activities: Dict[str, set]):
+    activity_key = 'concept:name'
+    resource_key = 'org:resource'
+    start_time_key = 'start_timestamp'
+    end_time_key = 'time:timestamp'
+
+    case = case.sort_values(by=['time:timestamp', 'start_timestamp']).copy()
+    case.reset_index()
+
+    # NOTE: we need 4 consecutive events to identify the ping-pong pattern: A→B→C→D
+
+    # tracking two events in past (e.g., in past: A, B, current: C, future: D)
+    before_previous_event: Optional[pd.Series] = None
+    previous_event: Optional[pd.Series] = None
+    # tracking if A and C have the same activity and resource
+    first_pair_same: bool = False
+    # tracking if B and D have the same activity and resource
+    second_pair_same: bool = False
+    # collecting all ping-pongs
+    ping_pongs = []
+    for i in case.index:
+        event = case.loc[i]  # current event C
+
+        if previous_event is None:
+            previous_event = event  # tracking the previous event B
+            continue
+
+        # parallelism identification
+        parallel: bool = parallel_activities.get(event[activity_key], False)
+
+        # handoff identification
+        consecutive_timestamps = previous_event[end_time_key] <= event[start_time_key]
+        activity_changed = previous_event[activity_key] != event[activity_key]
+        resource_changed = previous_event[resource_key] != event[resource_key]
+        handoff_occurred = False
+        if consecutive_timestamps and activity_changed and resource_changed and not parallel:
+            handoff_occurred = True
+
+        if not handoff_occurred or before_previous_event is None:
+            before_previous_event = previous_event  # tracking the event before previous A
+            previous_event = event  # tracking the previous event B
+            continue
+
+        # evaluating pairs: A and C, B and D
+        same_activity = before_previous_event[activity_key] == event[activity_key]
+        same_resource = before_previous_event[resource_key] == event[resource_key]
+        if same_activity and same_resource and not first_pair_same:
+            first_pair_same = True
+        elif same_activity and same_resource and first_pair_same:
+            second_pair_same = True
+
+        # ping-pong identification
+        if first_pair_same and second_pair_same:
+            ping_pongs.append({
+                'source_activity': previous_event[activity_key],
+                'source_resource': previous_event[resource_key],
+                'destination_activity': event[activity_key],
+                'destination_resource': event[resource_key]
+            })
+            first_pair_same = False
+            second_pair_same = False
+
+        before_previous_event = previous_event  # tracking the event before previous A
+        previous_event = event  # tracking the previous event B
+
+    return ping_pongs
 
 
 def _identify_handoffs_per_case(case: pd.DataFrame, parallel_activities: Dict[str, set]):
--- tests/test_handoff.py
+++ tests/test_handoff.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 from typing import List
 
 import pandas as pd
 import pytest
 
-from waste import handoff
+from waste import handoff, core
 
 
 @pytest.fixture
@@ -27,3 +28,51 @@ def test_negative_duration(assets_path):
     log_path = assets_path / 'PurchasingExample.xes'
     result = handoff.identify(log_path)
     assert sum(result['duration_sum_seconds'] < 0) == 0
+
+
+def test_ping_pong_identification(assets_path):
+    args = [
+        {
+            'name': 'A',
+            'log_path': assets_path / 'PurchasingExample.xes',
+            'case_path': assets_path / 'ping_pong_case.csv',
+            'expected': [{'source_activity': 'Create Request for Quotation',
+                          'source_resource': 'Kim Passa',
+                          'destination_activity': 'Analyze Request for Quotation',
+                          'destination_resource': 'Karel de Groot'}]
+        },
+        {
+            'name': 'B',
+            'log_path': assets_path / 'PurchasingExample.xes',
+            'case_path': assets_path / 'ping_pong_case_fake.csv',
+            'expected': []
+        },
+        {
+            'name': 'C',
+            'log_path': assets_path / 'PurchasingExample.xes',
+            'case_path': assets_path / 'double_ping_pong_case.csv',
+            'expected': [{'source_activity': 'Create Request for Quotation',
+                          'source_resource': 'Kim Passa',
+                          'destination_activity': 'Analyze Request for Quotation',
+                          'destination_resource': 'Karel de Groot'},
+                         {'source_activity': 'Approve Purchase Order for payment',
+                          'source_resource': 'Karel de Groot',
+                          'destination_activity': 'Send Invoice',
+                          'destination_resource': 'Kiu Kan'}]
+        }
+    ]
+
+    for arg in args:
+        print(f"\rTest case {arg['name']}")
+        log_path: Path = arg['log_path']
+        case_path: Path = arg['case_path']
+        expected = arg['expected']
+
+        log = core.lifecycle_to_interval(log_path)
+        parallel_activities = core.parallel_activities_with_heuristic_oracle(log)
+
+        case = pd.read_csv(case_path)
+        assert case is not None and len(case) > 0
+
+        result = handoff._identify_ping_pongs_per_case(case, parallel_activities)
+        assert result == expected
