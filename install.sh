26fa6dd032a0e6be14bdbd7a9de18516d1100085
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-04-29 17:51:05 +0300

    fix: Fixing CI to install even more R dependencies

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+# Python dependencies
 python3 -m venv venv
 source venv/bin/activate
 pip install poetry
 pip install .
-
 cd vendor/start-time-estimator; pip install -e .; cd ../..
 cd vendor/batch-processing-analysis; pip install -e .; cd ../..
 cd vendor/Prosimos; pip install -e .; cd ../..
-
 poetry build
+
+# R dependencies
+Rscript -e 'install.packages("lubridate")'
+Rscript -e 'install.packages("stringr")'
+Rscript -e 'install.packages("tidyr")'
+Rscript -e 'install.packages("arules")'
+Rscript -e 'install.packages("arulesSequences")'
+Rscript -e 'install.packages("bupaR")'
+Rscript -e 'install.packages("readr", repos="http://cran.us.r-project.org")'
+Rscript -e 'install.packages("vendor/bama", repos=NULL, type="source")'
