10b4c5c6c65498eae536b5000272a10947192980
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-11-21 15:47:21 +0200

    Build scripts version updates

--- build_push.sh
+++ build_push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-VERSION_RULE=$1
+#VERSION_RULE=$1
 
-[ -z "$VERSION_RULE" ] && echo "Version rule is empty" && exit 1
+#[ -z "$VERSION_RULE" ] && echo "Version rule is empty" && exit 1
 
 poetry update
 #poetry version $VERSION_RULE  # TODO: this should be committed before pushing
