af3d9a6dac79ea6ac3dd14b92e6d892fc522c94a
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-03-28 16:58:40 +0300

    Switching to Poetry completely without using git submodules

--- install_python.sh
+++ install_python.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-rm -rf venv
-python3 -m venv venv
-source venv/bin/activate
 pip install poetry
-pip install -e .
-cd vendor/start-time-estimator; pip install -e .; cd ../..
-cd vendor/batch-processing-discovery; pip install -e .; cd ../..
-cd vendor/Prosimos; pip install -e .; cd ../..
-poetry build
+poetry install
