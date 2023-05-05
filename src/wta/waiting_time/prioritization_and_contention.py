66b1a345d15d8048fa4b2201309c4a0986c83908
powermaxz <maxavramenko2018@gmail.com>
2023-05-05 08:51:11 +0300

    minor changes to prioritization_and_contention.py to improve performance

--- src/wta/waiting_time/prioritization_and_contention.py
+++ src/wta/waiting_time/prioritization_and_contention.py
@@ -8,14 +8,17 @@ from wta.helpers import EventLogIDs
 from wta.waiting_time.resource_unavailability import other_processing_events_during_waiting_time_of_event
 
 
-def detect_intervals(processing_events: pd.DataFrame, actual_event_enabled_time: pd.Series, log_ids: EventLogIDs, event: pd.DataFrame) -> Tuple[List[List], List[List]]:
-    actual_event_enabled_time_utc = pd.to_datetime(actual_event_enabled_time[0], utc=True)
+def detect_intervals(processing_events: pd.DataFrame, actual_event_enabled_time: pd.Timestamp, log_ids: EventLogIDs, event: pd.DataFrame) -> Tuple[List[List], List[List]]:
+    actual_event_enabled_time_utc = pd.to_datetime(actual_event_enabled_time, utc=True)
     events_due_to_prioritization = processing_events[processing_events[log_ids.enabled_time] > actual_event_enabled_time_utc]
     events_due_to_contention = processing_events[processing_events[log_ids.enabled_time] <= actual_event_enabled_time_utc]
 
     def calculate_intervals(events_due_to, actual_event_enabled_time):
         if events_due_to.size > 0:
-            start_time = np.maximum(actual_event_enabled_time, events_due_to[log_ids.start_time].values)
+            start_time = np.maximum(
+                np.array([actual_event_enabled_time], dtype='datetime64[ns]'),
+                events_due_to[log_ids.start_time].to_numpy(dtype='datetime64[ns]')
+            )
             end_time = np.minimum(event[log_ids.start_time].values, events_due_to[log_ids.end_time].values)
             intervals = [(start, end) for start, end in zip(start_time, end_time) if start <= end]
             return [list(t) for t in zip(*intervals)] if intervals else ([], [])
@@ -40,32 +43,30 @@ def detect_contention_and_prioritization_intervals(
 
     other_processing_events = other_processing_events_during_waiting_time_of_event(event_index, log, log_ids=log_ids)
 
-    event_batch_instance_id = event[log_ids.batch_id].values[0]
-    other_processing_events_in_batch = other_processing_events.query(
-        f'{log_ids.batch_id} == @event_batch_instance_id')
+    event_batch_instance_id = event.at[event.index[0], log_ids.batch_id]
+    other_processing_events_in_batch = other_processing_events.query(f'{log_ids.batch_id} == @event_batch_instance_id')
 
-    other_processing_events_out_batch = pd.concat([
-        other_processing_events,
-        other_processing_events_in_batch
-    ]).drop_duplicates(keep=False)
+    other_processing_events_out_batch = other_processing_events.merge(
+        other_processing_events_in_batch, indicator=True, how='outer').query('_merge == "left_only"').drop(columns='_merge')
 
-    actual_event_enabled_time = event[log_ids.enabled_time].values
+    actual_event_enabled_time = event.at[event.index[0], log_ids.enabled_time]
     wt_contention_intervals_in_batch, wt_prioritization_intervals_in_batch = detect_intervals(
         other_processing_events_in_batch, actual_event_enabled_time, log_ids, event)
 
-    actual_event_enabled_time = min(event[log_ids.batch_instance_enabled].values, event[log_ids.start_time].values)
-    if pd.isna(actual_event_enabled_time).all():
-        actual_event_enabled_time = event[log_ids.enabled_time].values
+    actual_event_enabled_time = min(event.at[event.index[0], log_ids.batch_instance_enabled],
+                                    event.at[event.index[0], log_ids.start_time])
+    if pd.isna(actual_event_enabled_time):
+        actual_event_enabled_time = event.at[event.index[0], log_ids.enabled_time]
     wt_contention_intervals_out_batch, wt_prioritization_intervals_out_batch = detect_intervals(
         other_processing_events_out_batch, actual_event_enabled_time, log_ids, event)
 
     wt_contention_intervals = (
-        list(wt_contention_intervals_in_batch[0]) + list(wt_contention_intervals_out_batch[0]),
-        list(wt_contention_intervals_in_batch[1]) + list(wt_contention_intervals_out_batch[1])
+        wt_contention_intervals_in_batch[0] + wt_contention_intervals_out_batch[0],
+        wt_contention_intervals_in_batch[1] + wt_contention_intervals_out_batch[1]
     )
 
     wt_prioritization_intervals = (
-        list(wt_prioritization_intervals_in_batch[0]) + list(wt_prioritization_intervals_out_batch[0]),
-        list(wt_prioritization_intervals_in_batch[1]) + list(wt_prioritization_intervals_out_batch[1])
+        wt_prioritization_intervals_in_batch[0] + wt_prioritization_intervals_out_batch[0],
+        wt_prioritization_intervals_in_batch[1] + wt_prioritization_intervals_out_batch[1]
     )
     return wt_contention_intervals, wt_prioritization_intervals
